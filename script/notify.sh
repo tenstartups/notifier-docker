5c0ee4a9d40657c9388abbaa331bb79778df7b35
Marc Lennox <marc.lennox@gmail.com>
2015-02-12 20:22:46 -0500

    Variable name change

--- script/notify.sh
+++ script/notify.sh
@@ -3,7 +3,12 @@ set -e
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
+# Warn if not notifier services present
+if [ -z "${NOTIFIER_SERVICES}" ]; then
+  echo "No notifier services specified; use the NOTIFIER_SERVICES environment variable to set them"
+fi
+
 # Send to each service requested
-if [[ "${NOTIFY_TRANSPORTS}" =~ slack   ]]; then "${SCRIPT_DIR}/slack-notify"   "$@"; fi
-if [[ "${NOTIFY_TRANSPORTS}" =~ hipchat ]]; then "${SCRIPT_DIR}/hipchat-notify" "$@"; fi
-if [[ "${NOTIFY_TRANSPORTS}" =~ mailgun ]]; then "${SCRIPT_DIR}/mailgun-notify" "$@"; fi
+if [[ "${NOTIFIER_SERVICES}" =~ slack   ]]; then "${SCRIPT_DIR}/slack-notify"   "$@"; fi
+if [[ "${NOTIFIER_SERVICES}" =~ hipchat ]]; then "${SCRIPT_DIR}/hipchat-notify" "$@"; fi
+if [[ "${NOTIFIER_SERVICES}" =~ mailgun ]]; then "${SCRIPT_DIR}/mailgun-notify" "$@"; fi
